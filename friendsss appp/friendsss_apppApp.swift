578083526cb750371221375c848a3d5307089d41
mena <stuf408478@ese.gov.ae>
2023-08-31 07:45:37 +0400

    Initial Commit

---
+++ friendsss appp/friendsss_apppApp.swift
@@ -0,0 +1,17 @@
+//
+//  friendsss_apppApp.swift
+//  friendsss appp
+//
+//  Created by Mena Haitham on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct friendsss_apppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
